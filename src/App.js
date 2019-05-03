50d19947f6c3bc5ab103b9c0e90efda39f0e9c69
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-05-03 15:04:13 -0500

    Fixed styling for main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="app-container"> 
+        <div>
+          <div>
+            <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Karma" rel="stylesheet"/>
+          </div> 
+          <div>
+            <h1 className="Bd">The Dream Is Still Alive</h1> 
+          </div>
+        </div>
+        <div id="main-container">
+          <div className="sub-container">
+            <div >
+              <h2 className="identifier">_MALE_</h2>
+              <div id="minContainer">
+                <img  src="https://vignette.wikia.nocookie.net/gotascent/images/4/4e/Promotional_Art_-_Warrior_King.jpg/revision/latest?cb=20130517061037" height="350" width="250" alt=" " />
+              </div>
+              <h3 className="indicator" > CLICK IMAGE</h3> 
+            </div> 
+          </div>
+          <div className="sub-container">
+            <div > 
+              <h2 className="identifier">FEMALE</h2>      
+              <div id="minContainer"> 
+                <img  src="https://i.pinimg.com/originals/eb/4b/56/eb4b56205c9ed5cd384cde2b1962c34b.jpg" height="350" width="250" alt=" " />
+              </div>
+              <h3 className="indicator" > CLICK IMAGE</h3>
+            </div>
+          </div>
+        </div>
+          {/*<Warrior />*/}
       </div>
     );
   }
---
+++ src/components/Warrior.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const warrior = () => {
+  return (
+    <center>
+  <p>
+  <br/>
+  <br/>What defines a warrior</p>
+  </center>
+ )
+};
+
+export default warrior;
