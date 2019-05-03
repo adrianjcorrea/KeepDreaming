2f02d0cf9c0f71e9dceb0adf3c6220be7fffaa62
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-05-03 15:24:13 -0500

    added event listener onClickImage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import './App.css';
 
 
 class App extends Component {
+
+  onClickImage = () => {
+    alert('I am functional');
+  }
+
+
   render() {
     return (
       <div id="app-container"> 
@@ -21,7 +27,12 @@ class App extends Component {
             <div >
               <h2 className="identifier">_MALE_</h2>
               <div id="minContainer">
-                <img  src="https://vignette.wikia.nocookie.net/gotascent/images/4/4e/Promotional_Art_-_Warrior_King.jpg/revision/latest?cb=20130517061037" height="350" width="250" alt=" " />
+                <img  src="https://vignette.wikia.nocookie.net/gotascent/images/4/4e/Promotional_Art_-_Warrior_King.jpg/revision/latest?cb=20130517061037" 
+                  height="350" 
+                  width="250" 
+                  alt=" "
+                  onClick={this.onClickImage} 
+                />
               </div>
               <h3 className="indicator" > CLICK IMAGE</h3> 
             </div> 
@@ -30,7 +41,12 @@ class App extends Component {
             <div > 
               <h2 className="identifier">FEMALE</h2>      
               <div id="minContainer"> 
-                <img  src="https://i.pinimg.com/originals/eb/4b/56/eb4b56205c9ed5cd384cde2b1962c34b.jpg" height="350" width="250" alt=" " />
+                <img src="https://i.pinimg.com/originals/eb/4b/56/eb4b56205c9ed5cd384cde2b1962c34b.jpg" 
+                  height="350" 
+                  width="250" 
+                  alt=" " 
+                  onClick={this.onClickImage} 
+                />
               </div>
               <h3 className="indicator" > CLICK IMAGE</h3>
             </div>
